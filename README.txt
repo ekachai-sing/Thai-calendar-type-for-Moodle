0386ffc19d0a920798a8e8399289aa17d2e53ea2
ekachai-sing <ekachai.singthong@gmail.com>
2014-09-29 15:45:16 +0700

    V 1.1

---
+++ README.txt
@@ -0,0 +1,24 @@
+The Thai calendar is a solar calendar which use Buddish year, Thai month name, Thai date name, the official calendar in Thailand.
+
+The calendar type system was introduced in 2.6, so this will not work in any Moodle versions earlier than this.
+
+To install this calendar type do either of the folllowing steps.
+
+	1. Download and extract the zip, rename the folder to thai and place this folder in the calendar/type folder in your Moodle directory.
+	2. Visit http://YOURSITEMAINURL/admin/index.php and update database.
+
+Credit : This calendar's source code get ideas from Jalali calendar.
+
+
+
+------------------------------------------ ภาษาไทย -----------------------------------------
+ปฏิทินไทย เป็นปฏิทินตามสุริยคติ ใช้ปี พ.ศ. เดือนชื่อไทย วันชื่อไทย ซึ่งเป็นปฏิทินใช้ตามราชการไทย
+
+ปฏิทินนี้เริ่มต้นใช้ได้กับ Moodle 2.6 ขึ้นไปเท่านั้น เวอร์ชั่นก่อนหน้าจะไม่สามารถใช้งานได้
+
+การติดตั้งให้ปฏิบัติดังนี้
+
+	1. ดาวน์โหลดและแตกไฟล์ แล้วสร้างโฟลเดอร์ชื่อ thai ไว้ภายใต้โฟลเดอร์ calendar/type ของ Moodle
+	2. ไปที่  http://YOURSITEMAINURL/admin/index.php  จากนั้นทำการอัพเดทระบบ
+
+เครดิต : โค้ดของปฏิทินนี้ ได้แนวคิดมาจากปฏิทิน Jalali
---
+++ lang/en/calendartype_thai.php
@@ -0,0 +1,57 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'calendartype_gregorian', language 'en'.
+ *
+ * @package calendartype_thai
+ * @author Ekachai Singthong {@link ekachai.singthong@gmail.com}
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['name'] = 'Thai';
+$string['pluginname'] = 'Thai calendar type';
+
+$string['am'] = 'am';
+$string['am_caps'] = 'AM';
+$string['month1'] = 'มกราคม';
+$string['month2'] = 'กุมภาพันธ์';
+$string['month3'] = 'มีนาคม';
+$string['month4'] = 'เมษายน';
+$string['month5'] = 'พฤษภาคม';
+$string['month6'] = 'มิถุนายน';
+$string['month7'] = 'กรกฎาคม';
+$string['month8'] = 'สิงหาคม';
+$string['month9'] = 'กันยายน';
+$string['month10'] = 'ตุลาคม';
+$string['month11'] = 'พฤศจิกายน';
+$string['month12'] = 'ธันวาคม';
+$string['pm'] = 'pm';
+$string['pm_caps'] = 'PM';
+$string['wday0'] = 'อา';
+$string['wday1'] = 'จ';
+$string['wday2'] = 'อ';
+$string['wday3'] = 'พ';
+$string['wday4'] = 'พฤ';
+$string['wday5'] = 'ศ';
+$string['wday6'] = 'ส';
+$string['weekday0'] = 'อาทิตย์';
+$string['weekday1'] = 'จันทร์';
+$string['weekday2'] = 'อังคาร';
+$string['weekday3'] = 'พุธ';
+$string['weekday4'] = 'พฤหัสบดี';
+$string['weekday5'] = 'ศุกร์';
+$string['weekday6'] = 'เสาร์';
